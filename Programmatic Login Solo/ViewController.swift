b36dc54cb4df2f6fd1458a345dc1b0a3e35d40c0
Marc <marcjacques@me.com>
2021-12-21 12:58:08 -0500

    Add facebookbranding to stackview

--- Programmatic Login Solo/ViewController.swift
+++ Programmatic Login Solo/ViewController.swift
@@ -9,25 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    private let usernameTextField: UITextField = {
-//        let textfield = UITextField()
-//
-//        return textfield
-//    }
-//
+    
+    private let facebookBrand: UIImageView = {
+        let imageName = "facebookLogo"
+        let image = UIImage(named: imageName)
+        let imageView = UIImageView(image: image!)
+        imageView.frame = CGRect(x: 0, y: 0, width: 100, height: 200)
+        return imageView
+    }()
+    
+    //    private let usernameTextField: UITextField = {
+    //        let textfield = UITextField()
+    //
+    //        return textfield
+    //    }()
+    //
     //private let passwordTextField: UITextField = {
-//}
-
-//    private let loginButton: UIButton = {
-//
-//    }
+    //}()
+    
+    //    private let loginButton: UIButton = {
+    //
+    //    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 0.26, green: 0.40, blue: 0.70, alpha: 1.00)
-//        (red: 66/255.0, green: 103/255.0, blue: 178/255.0, alpha: 0.0)
+        view.addSubview(facebookBrand)
     }
+    
+    func setupStackView() {
+        var stackView: UIStackView = {
+            let stack = UIStackView()
+            stack.axis = .vertical
+            stack.spacing = 20.0
+            stack.alignment = .center
+            stack.distribution = .fillEqually
+            [self.facebookBrand] { stack.addArrangedSubview($0) }
+            return stack
+        }()
+    }
+}
 
 
 
