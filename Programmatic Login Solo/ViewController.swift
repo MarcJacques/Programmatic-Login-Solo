b83aa9a7d3adfc6282a2d0766cb4f48d2a5eb305
Marc <marcjacques@me.com>
2021-12-21 12:32:14 -0500

    Set background color to facebook signature blue

--- Programmatic Login Solo/ViewController.swift
+++ Programmatic Login Solo/ViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    private let usernameTextField: UITextField = {
+//        let textfield = UITextField()
+//
+//        return textfield
+//    }
+//
+    //private let passwordTextField: UITextField = {
+//}
 
+//    private let loginButton: UIButton = {
+//
+//    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = UIColor(red: 0.26, green: 0.40, blue: 0.70, alpha: 1.00)
+//        (red: 66/255.0, green: 103/255.0, blue: 178/255.0, alpha: 0.0)
     }
+
 
 
 }
